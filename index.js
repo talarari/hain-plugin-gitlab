e50e7ca8d7aa911800d52b7b26927c1e1b41d23e
tal <tal@soluto.com>
2016-06-18 00:21:54 +0300

    added fake result  to direct user to enter creds . added error result to direct user to correct creds.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = (pluginContext) => {
     const app = pluginContext.app;
     const shell = pluginContext.shell;
     const logger = pluginContext.logger;
-    const preferences = pluginContext.preferences.get();
+    const preferences = pluginContext.preferences;
 
     const TIMEOUT = 200;
     const FAKE = "FAKE_ID";
@@ -12,12 +12,18 @@ module.exports = (pluginContext) => {
     var gitlab = require('node-gitlab');
     var client;
 
+    const initClient = (prefs)=>{
+        if (prefs.gitlabInstallation && prefs.privateToken){
+            client = gitlab.create({
+                api: prefs.gitlabInstallation + '/api/v3',
+                privateToken: prefs.privateToken
+            });
+        }
+    };
 
     function startup() {
-        client = gitlab.create({
-            api: preferences.gitlabInstallation + '/api/v3',
-            privateToken: preferences.privateToken
-        });
+        initClient(preferences.get());
+        preferences.on('update',initClient);
     }
 
     function debounce(pFunction) {
@@ -34,16 +40,28 @@ module.exports = (pluginContext) => {
 
 
     function queryProjects(pSearchString, pRes) {
+        const prefs =preferences.get();
         pRes.add({
             id: FAKE,
             title: 'fetching...',
-            desc: 'from ' + preferences.gitlabInstallation,
+            desc: 'from ' + prefs.gitlabInstallation,
             icon: '#fa fa-spinner fa-spin'
         });
         client.projects.search({
             query: pSearchString
         }, function(err, projects) {
             pRes.remove(FAKE);
+
+            if (err){
+                pRes.add({
+                    id: FAKE,
+                    title: "Oops, could'nt get your results",
+                    desc: "Make sure plugin preferences are correct, Click here to check",
+                    icon :"#fa fa-exclamation-circle",
+                    payload:{action:'prefs'}
+                });
+                return;
+            }
             if (projects) {
                 for (var i = 0; i < projects.length; i++) {
                     const project = projects[i];
@@ -51,8 +69,8 @@ module.exports = (pluginContext) => {
                         id: project.id,
                         title: project.name_with_namespace,
                         desc: project.description,
-                        payload: project.web_url,
-                        icon: project.avatar_url ? project.avatar_url + '?private_token=' + preferences.privateToken : "#fa fa-product-hunt"
+                        payload: {action:'open',url:project.web_url},
+                        icon: project.avatar_url ? project.avatar_url + '?private_token=' + prefs.privateToken : "#fa fa-product-hunt"
                     };
                     pRes.add(projectResponse);
                 }
@@ -68,6 +86,23 @@ module.exports = (pluginContext) => {
     }
 
     function search(pSearchString, res) {
+
+        if (!client){
+            res.add({
+                id: FAKE,
+                title: "Please enter gitlab url and token",
+                desc: "Click this to open preferences",
+                icon :"#fa fa-unlock-alt",
+                payload:{
+                    action:'prefs'
+                }
+            });
+            return;
+        }
+        else {
+            res.remove(FAKE);
+        }
+
         const searchString = pSearchString.trim();
         if (searchString.length == 0) {
             res.add({
@@ -82,10 +117,21 @@ module.exports = (pluginContext) => {
     }
 
     function execute(id, payload) {
+        logger.log(JSON.stringify(payload))
         if (payload) {
-            shell.openExternal(payload);
+            switch (payload.action){
+                case 'prefs':{
+                    app.openPreferences('hain-plugin-gitlab');
+                    break;
+                }
+                case 'open':{
+                    shell.openExternal(payload.url);
+                    app.close();
+                    break;
+                }
+
+            }
         }
-        app.close();
     }
 
     return {
